b3c749bfba82d92fb939d200d119712164c020d9
ToshiroAkihabara <toshiro.akihabara@gmail.com>
2024-02-23 00:15:44 +0300

    Queue

---
+++ Queue/queue.py
@@ -0,0 +1,25 @@
+from collections import deque
+import logging
+
+
+class FIFO:
+    def __init__(self) -> None:
+        logging.basicConfig(level=logging.DEBUG)
+        self.queue = deque([])
+    
+    def append(self, item: int) -> None:
+        self.queue.append(item)
+        logging.debug(f"Inserted a {item} into the queue")
+        
+    def pop(self) -> int:
+        if not self.empty():
+            item = self.queue.popleft()
+            logging.debug(f"Got an element {item} from the queue")
+            return item
+        logging.debug("Queue is empty")
+        
+    def empty(self) -> bool:
+        return len(self.queue) == 0
+
+    def __str__(self):
+        return self.queue
