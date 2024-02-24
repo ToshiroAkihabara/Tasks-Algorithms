6301b7c6bc3cd3d5781e357fb43a835886d7cc1a
ToshiroAkihabara <toshiro.akihabara@gmail.com>
2024-02-24 13:50:18 +0300

    Added tests exceptions

--- Matrix_Vectors/matrix.py
+++ Matrix_Vectors/matrix.py
@@ -4,20 +4,31 @@ import numpy as np
 class Matrix:
     @classmethod
     def matrix_sum(
-            cls, matrix_1: np.array([], int),
-            matrix_2: np.array([], int)
-    ) -> np.array([], int):
-        return matrix_1 + matrix_2
-
-    @classmethod
-    def matrix_mult(
             cls,
             matrix_1: np.array([], int),
             matrix_2: np.array([], int)
     ) -> np.array([], int):
-        if len(matrix_1[0]) == len(matrix_2[:, 0]):
-            return np.dot(matrix_1, matrix_2)
+        if isinstance(matrix_1, np.ndarray) and isinstance(matrix_2, np.ndarray):
+            return matrix_1 + matrix_2
         else:
-            raise ValueError(f"The length {matrix_1} of row not equal the length {matrix_2} of column")
+            raise ValueError("Unsupported type. Convert data to numpy.ndarray")
+
+    @classmethod
+    def matrix_mult(
+            cls,
+            matrix_1: np.array([], dtype=int),
+            matrix_2: np.array([], dtype=int)
+    ) -> np.array([], int):
+        if isinstance(matrix_1, np.ndarray) and isinstance(matrix_2, np.ndarray):
+            if len(matrix_1[0]) == len(matrix_2[:, 0]):
+                try:
+                    result = np.dot(matrix_1, matrix_2)
+                    return result
+                except ValueError:
+                    raise ValueError("Data type must provide an itemsize")
+            else:
+                raise ValueError(f"The length {matrix_1} of row not equal the length {matrix_2} of column")
+        else:
+            raise ValueError("Unsupported type. Convert data to numpy.ndarray")
 
 
--- Matrix_Vectors/tests/test_matrix.py
+++ Matrix_Vectors/tests/test_matrix.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pytest
-
+from contextlib import nullcontext as does_not_raises
 from Matrix_Vectors.matrix import Matrix
 
 
@@ -11,10 +11,29 @@ def matx():
 
 
 class TestMatrix:
-    @classmethod
-    def test_matrix_sum(cls, matx: Matrix) -> None:
-        assert (matx.matrix_sum(np.array([1, 4, 8]), np.array([10, 3, 6])) == np.array([11, 7, 14])).all()
+    @pytest.mark.parametrize(
+        "matrix_1, matrix_2, expected_result, exception",
+        [
+            (np.array([[1, 8], [2, 5]], dtype=int), np.array([[5, 6], [10, 9]], dtype=int),
+             np.array([[85, 78], [60, 57]], dtype=int), does_not_raises()),
+            (np.array([[1, 8], [2, 5]], dtype=int), np.array([[5, 6], [10, 9], [5, 2]], dtype=int),
+             np.array([[85, 78], [60, 57]], dtype=int), pytest.raises(ValueError)),
+            ([[1, 8], [2, 5]], [[5, 6], [10, 9]], [[85, 78], [60, 57]], pytest.raises(ValueError))
+        ]
+    )
+    def test_matrix_mult(self, matx: Matrix, matrix_1, matrix_2, expected_result, exception) -> None:
+        with exception:
+            assert (matx.matrix_mult(matrix_1, matrix_2) == expected_result).all()
 
-    @classmethod
-    def test_matrix_mult(cls, matx: Matrix) -> None:
-        assert (matx.matrix_mult(np.array([[1, 8], [2, 5]]), np.array([[5, 6], [10, 9]])) == [[85, 78], [60, 57]]).all()
+    @pytest.mark.parametrize(
+        "matrix_1, matrix_2, expected_result, exception",
+        [
+            (np.array([1, 4, 8], dtype=int), np.array([10, 3, 6], dtype=int), np.array([11, 7, 14], dtype=int),
+             does_not_raises()),
+            ([6, 10, 23], np.array([2, 14, 9], dtype=int), np.array([8, 24, 32], dtype=int),
+             pytest.raises(ValueError))
+        ]
+    )
+    def test_matrix_sum(self, matx: Matrix, matrix_1, matrix_2, expected_result, exception) -> None:
+        with exception:
+            assert (matx.matrix_sum(matrix_1, matrix_2) == expected_result).all()
